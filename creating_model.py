b752def7ecad4cb6851b1d3d23c27416603863a4
SamikshaSam52 <samikshaanand28@gmail.com>
2025-05-14 21:15:05 +0530

    second commit

--- creating_model.py
+++ creating_model.py
@@ -1,4 +1,5 @@
-def create_model(input_shape=(128,128,3),num_classes=2:
+def the
+create_model(input_shape=(128,128,3),num_classes=2:
                  model = Sequential([
                      conv2d(32,(3,3),
 
